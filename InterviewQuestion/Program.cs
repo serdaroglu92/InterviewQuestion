4da468e14a1bc4fbf9f6c3d6050d0811d667c9d6
Yusuf Ozturk <yusuf.ozturk@outlook.com>
2020-07-21 14:14:19 +0200

    Data validation added

--- InterviewQuestion/Program.cs
+++ InterviewQuestion/Program.cs
@@ -29,7 +29,7 @@ namespace InterviewQuestion
             ////////////////////////////////////
             /// Place your answer before this section
             ///////////////////////////////////
-
+            
             // Stop Timer
             stopWatch.Stop();
 
@@ -40,6 +40,40 @@ namespace InterviewQuestion
             string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
                 ts.Hours, ts.Minutes, ts.Seconds,
                 ts.Milliseconds / 10);
+
+            ////////////////////////////
+            // Data Validation
+            ////////////////////////////
+            
+            if (queryResultTables[0].column1 != "800")
+            {
+                Console.WriteLine("ERROR IN CALCULATION for column1!");
+            }
+
+            if (queryResultTables[0].column2 != "160")
+            {
+                Console.WriteLine("ERROR IN CALCULATION for column2!");
+            }
+
+            if (queryResultTables[0].column3 != "180")
+            {
+                Console.WriteLine("ERROR IN CALCULATION for column3!");
+            }
+
+            if (queryResultTables[0].column4 != "320")
+            {
+                Console.WriteLine("ERROR IN CALCULATION for column4!");
+            }
+
+            if (queryResultTables[0].column5 != "780")
+            {
+                Console.WriteLine("ERROR IN CALCULATION for column5!");
+            }
+
+            if (queryResultTables[0].column6 != "15")
+            {
+                Console.WriteLine("ERROR IN CALCULATION for column6!");
+            }
 
             Console.WriteLine("Execution duration: " + elapsedTime);
 
